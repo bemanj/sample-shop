e7f4b9212cbf1a3c6f05c9207ca1c0e8379b78a1
Robert John Millano <bemanj87@gmail.com>
2017-10-27 18:40:56 +0800

    linting

--- src/app/shared/services/auth-guard.service.ts
+++ src/app/shared/services/auth-guard.service.ts
@@ -2,17 +2,19 @@ import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { CanActivate, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map'; 
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
-  
+
   canActivate(route, state: RouterStateSnapshot) {
     return this.auth.user$.map(user => {
-      if (user) return true; 
-      console.log("aug ");
+      if (user) {
+        return true;
+      }
+      console.log('aug ');
       this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
       return false;
     });
