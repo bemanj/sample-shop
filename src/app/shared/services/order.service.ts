cb23a326e852e31508ce4e3066375b9fd89834b2
Robert John Millano <bemanj87@gmail.com>
2017-10-27 18:57:00 +0800

    remove firebase

--- src/app/shared/services/order.service.ts
+++ src/app/shared/services/order.service.ts
@@ -1,65 +1,65 @@
 import { forEach } from '@angular/router/src/utils/collection';
 import { ShoppingCartService } from './shopping-cart.service';
-import { AngularFireDatabase } from 'angularfire2/database';
+// import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class OrderService {
 totalSales = 0 ;
 
-  constructor(private db: AngularFireDatabase, private shoppingCartService: ShoppingCartService) { 
-  }
+  // constructor(private db: AngularFireDatabase, private shoppingCartService: ShoppingCartService) { 
+  // }
 
-  async placeOrder(order) {
-    let result = await this.db.list('/orders').push(order);
-    this.shoppingCartService.clearCart();
-    return result;
-  }
+  // async placeOrder(order) {
+  //   let result = await this.db.list('/orders').push(order);
+  //   this.shoppingCartService.clearCart();
+  //   return result;
+  // }
 
-  getOrders() { 
-    return this.db.list('/orders');
-  }
+  // getOrders() {
+  //   return this.db.list('/orders');
+  // }
 
-  getSomething() {
-    let item = 0;
-    return this.db.list('/orders', { preserveSnapshot: true})
-    .map(
-      async res => {
-        // do something here
-        res.forEach(res => {
-          return item += res.val()['grossAmount']
-        });
-      },
-      err => {
-            console.error(err);
-      }
-    );
-  }
+  // getSomething() {
+  //   let item = 0;
+  //   return this.db.list('/orders', { preserveSnapshot: true})
+  //   .map(
+  //     async res => {
+  //       // do something here
+  //       res.forEach(res => {
+  //         return item += res.val()['grossAmount']
+  //       });
+  //     },
+  //     err => {
+  //           console.error(err);
+  //     }
+  //   );
+  // }
 
-  getTotalSales()  { 
-    console.log ('test only ' + this.getSomething());
-    return this.totalSales;
-  }
+  // getTotalSales()  {
+  //   console.log ('test only ' + this.getSomething());
+  //   return this.totalSales;
+  // }
 
-  get(orderId) { 
-    return this.db.object('/orders/' + orderId);
-  }
+  // get(orderId) {
+  //   return this.db.object('/orders/' + orderId);
+  // }
 
-  getOrdersByOrderId(orderId: string) {
-    return this.db.list('/orders', {
-      query: {
-        orderByKey: true,
-        equalTo: orderId        
-      }
-    });
-  }
+  // getOrdersByOrderId(orderId: string) {
+  //   return this.db.list('/orders', {
+  //     query: {
+  //       orderByKey: true,
+  //       equalTo: orderId
+  //     }
+  //   });
+  // }
 
-  getOrdersByUser(userId: string) {
-    return this.db.list('/orders', {
-      query: {
-        orderByChild: 'userId',
-        equalTo: userId        
-      }
-    });
-  }
+  // getOrdersByUser(userId: string) {
+  //   return this.db.list('/orders', {
+  //     query: {
+  //       orderByChild: 'userId',
+  //       equalTo: userId
+  //     }
+  //   });
+  // }
 }
