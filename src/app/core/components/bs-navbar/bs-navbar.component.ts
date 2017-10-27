496873b0b8d030ba42ac7e3bdcaa0d9c267151f6
Robert John Millano <bemanj87@gmail.com>
2017-10-27 18:43:33 +0800

    linting

--- src/app/core/components/bs-navbar/bs-navbar.component.ts
+++ src/app/core/components/bs-navbar/bs-navbar.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from '../../../shared/services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'bs-navbar',
+  selector: 'app-bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
@@ -17,37 +17,37 @@ export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
   cart$: Observable<ShoppingCart>;
 
-  constructor(private auth: AuthService, 
-    private router: Router, 
+  constructor(private auth: AuthService,
+    private router: Router,
     private salesreportservice: SalesReportService,
-    private shoppingCartService: ShoppingCartService) { 
+    private shoppingCartService: ShoppingCartService) {
   }
 
-  async ngOnInit() { 
+  async ngOnInit() {
     this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
     this.cart$ = await this.shoppingCartService.getCart();
   }
-  
 
-  save(){
-    
-    var date = new Date();
 
-    var sodata = {
+  save() {
+
+    const date = new Date();
+
+    const sodata = {
       OrderDate: date,
       Customer: '',
-      SubTotal: 0,//orderHeader.soSubTotal,
-      TaxAmt: 0,//orderHeader.soTaxAmt,
-      Freight: 0,//orderHeader.soFreight,
-      Comment: 'test',//orderHeader.soComment,
+      SubTotal: 0, // orderHeader.soSubTotal,
+      TaxAmt: 0, // orderHeader.soTaxAmt,
+      Freight: 0, // orderHeader.soFreight,
+      Comment: 'test', // orderHeader.soComment,
       ModifiedDate: date
-    }
+    };
 
   this.salesreportservice.create(sodata).subscribe(data => {
     this.router.navigate(['/sales-order', data.SalesOrderID]);
       });
   }
-  
+
   logout() {
     this.auth.logout();
   }
