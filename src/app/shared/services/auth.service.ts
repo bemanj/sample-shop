952bf7cb2b513da37fc3f3611e7498b11bf07bd6
Robert John Millano <bemanj87@gmail.com>
2017-10-27 18:18:47 +0800

    changes

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -23,11 +23,11 @@ export class AuthService {
     let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     localStorage.setItem('returnUrl', returnUrl);
     
-    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    // this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
   logout() { 
-    this.afAuth.auth.signOut();
+    // this.afAuth.auth.signOut();
   }
 
   get appUser$() : Observable<AppUser> {
