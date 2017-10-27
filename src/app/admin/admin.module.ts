b105ca07808808da9e7c6ae8e755d845e4d5bd74
Robert John Millano <bemanj87@gmail.com>
2017-10-27 18:39:57 +0800

    remove auth in admin module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -18,28 +18,23 @@ import { ProductListComponent } from './components/product-list/product-list/pro
       {
         path: 'admin/products/new',
         component: ProductFormComponent,
-        canActivate: [AuthGuard, AdminAuthGuard]
       },
       {
         path: 'admin/products/:id',
         component: ProductFormComponent,
-        canActivate: [AuthGuard, AdminAuthGuard]
       },
       {
         path: 'admin/products',
         component: AdminProductsComponent,
-        canActivate: [AuthGuard, AdminAuthGuard]
       },
       {
         path: 'admin/product-list',
         component: ProductListComponent,
-        canActivate: [AuthGuard, AdminAuthGuard]
       },
 
       {
         path: 'admin/orders',
         component: AdminOrdersComponent,
-        canActivate: [AuthGuard, AdminAuthGuard]
       }
     ])
   ],
